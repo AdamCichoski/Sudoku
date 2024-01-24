02597ff88c0da4ab0f34b6b43370c85e056d6c28
Adam Cichoski <149607797+AdamCichoski@users.noreply.github.com>
2024-01-24 16:48:08 -0500

    Add files via upload

---
+++ Sudoku/src/sudokuBoard/Rectangle.java
@@ -0,0 +1,22 @@
+package sudokuBoard;
+
+public class Rectangle {
+    private float x;
+    private float y;
+    private float xLength;
+    private float yLength;
+
+    /**
+     * Constructor
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @param xLength the length of the rectanlge in terms of x length
+     * @param yLength the length of the rectangle in terms of y length
+     */
+    public Rectangle(float x, float y, float xLength, float yLength){
+        this.x = x;
+        this.y = y;
+        this.xLength = xLength;
+        this.yLength = yLength;
+    }
+}
