efaa44ec59d7e3063638c83b8407917b44c55ea5
Adam Cichoski <cjadam003@gmail.com>
2024-01-25 15:40:37 -0500

    This update added the needed PApplet functionality to the main class in order to allow for a frame to be created and run, it added the Board class which is used to create the layout of the Sudoku board, and the Rectangle class which will be used in later updates to provide functionality for other parts of the game.

--- Sudoku/src/Main.java
+++ Sudoku/src/Main.java
@@ -1,7 +1,26 @@
 package src;
+import processing.core.PApplet;
+import src.sudokuBoard.*;
+public class Main extends PApplet {
+    private Board board;
+    int x=0;
+    public void settings(){
+        board = new Board(this);
+        size((short)board.SCREEN_WIDTH,(short)board.SCREEN_HEIGHT);
+        if(x == 0){
+            board.printCurrent();
+            x++;
+        }
+    }
 
-public class Main {
-    public static void main(String[] args){
-        System.out.println("hello world");
+    public void draw(){
+        background(228466);
+
+        board.boardLayout();
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"src.Main"};
+        PApplet.main(input);
     }
 }
